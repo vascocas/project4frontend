0dfb3efa83629ed3e641f65e44464d1c79313fcb
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-03-11 15:58:11 +0000

    v1

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from "react";
 import { userStore } from "../stores/UserStore";
 import "./AddTaskForm.css";
 
-function AddTaskForm() {
+function AddTaskForm(onTaskAction) {
   const { token } = userStore();
   const [categories, setCategories] = useState([]);
   const [priority, setPriority] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [category, setCategory] = useState("");
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -38,6 +43,42 @@ function AddTaskForm() {
     setPriority(event.target.value);
   };
 
+  const handleAddTask = async () => {
+    try {
+      const requestBody = JSON.stringify({
+        "title": title,
+        "description": description,
+        "priority": priority,
+        "startDate": startDate,
+        "endDate": endDate,
+        "category": category,
+      });
+
+      const response = await fetch(
+        "http://localhost:8080/project4vc/rest/tasks/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            token: token,
+          },
+          body: requestBody,
+        }
+      );
+
+      if (response.ok) {
+        const successMessage = await response.text();
+        console.log(successMessage);
+        onTaskAction();
+      } else {
+        const errorMessage = await response.text();
+        console.error(errorMessage);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <aside className="add-task-sidebar">
       <div className="add-task-container">
@@ -48,28 +89,44 @@ function AddTaskForm() {
         <input
           type="text"
           id="taskName"
-          placeholder="required"
+          placeholder="Insert title"
           maxLength="20"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label className="labels-task-description" htmlFor="taskDescription">
           Description
         </label>
-        <textarea id="taskDescription" placeholder="required"></textarea>
+        <textarea
+          id="taskDescription"
+          placeholder="Insert description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
         <label className="labels-task-dates" htmlFor="startDate">
           Start date
         </label>
-        <input type="date" id="startDate" required />
+        <input
+          type="date"
+          id="startDate"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
         <label className="labels-task-dates" htmlFor="endDate">
           End date
         </label>
-        <input type="date" id="endDate" required />
+        <input
+          type="date"
+          id="endDate"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
         <label htmlFor="priority">Priority</label>
         <div className="dropdown-priority">
           <select
             id="dropdown-task-priority"
             value={priority}
-            onChange={handlePriorityChange}
-            required>
+            onChange={handlePriorityChange}>
             <option value="">Select priority</option>
             <option value="LOW_PRIORITY">Low</option>
             <option value="MEDIUM_PRIORITY">Medium</option>
@@ -78,7 +135,12 @@ function AddTaskForm() {
           </div>
           <div className="dropdown-category">
           <label className="labels-task-category" htmlFor="dropdown-task-categories">Category</label>
-          <select name="task-categories" id="dropdown-task-categories" required>
+          <select
+            name="task-categories"
+            id="dropdown-task-categories"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="">Select category</option>
             {categories.map((category) => (
               <option key={category.id} value={category.id}>
@@ -87,7 +149,7 @@ function AddTaskForm() {
             ))}
           </select>
           <div className="add-button">
-          <button id="addTask">Add task</button>
+          <button id="addTask" onClick={handleAddTask}>Add task</button>
           <p id="warningMessage2"></p>
           </div>
         </div>
