df5d2d55179ab95d7bba23a860a137618cfad707
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-03-21 16:13:22 +0000

    v1

--- src/components/tasks/TasksBoard.js
+++ src/components/tasks/TasksBoard.js
@@ -21,7 +21,7 @@ function TasksBoard() {
       return; // If token is not present, exit the function early
     }
     try {
-      let url = "http://localhost:8080/project4vc/rest/tasks/all";
+      let url = "http://localhost:8080/project4vc/rest/tasks";
       if (filteredUserId) {
         url += `/user/?userId=${filteredUserId}`;
       } else if (filteredCategoryId) {
